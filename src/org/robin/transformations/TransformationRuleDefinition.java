27520908c24747754e4125e9cfaf6df68e6d213f
Robin Sveningson <robinsveningson@gmail.com>
2018-12-10 10:29:35 +0100

    Remade rules to match method declarations and method calls instead.

--- src/org/robin/transformations/TransformationRuleDefinition.java
+++ src/org/robin/transformations/TransformationRuleDefinition.java
@@ -3,17 +3,11 @@ package org.robin.transformations;
 public class TransformationRuleDefinition {
     public static final TransformationRule[] TRANSFORMATIONS = new TransformationRule[] {
             new TransformationRule(
-                    "class ([a-z])([a-zA-Z]*)",
-                    SectionTransformation.byText("class "),
-                    SectionTransformation.byMethod(GroupTransformationMethod.UPPERCASE),
-                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE)
-            ),
-            new TransformationRule(
-                    "(int|String|double|boolean|char|long|float) ([A-Z])([a-zA-Z]*)",
+                    "([\\s\\.])([A-Z])([a-zA-Z0-9_]*)\\(",
                     SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
-                    SectionTransformation.byText(" "),
                     SectionTransformation.byMethod(GroupTransformationMethod.LOWERCASE),
-                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE)
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byText("(")
             )
     };
 }
