31371db09c2cd8aac8185c5a1aad4768c72a50cf
Robin Sveningson <robinsveningson@gmail.com>
2018-12-17 11:23:18 +0100

    Made the rules for matching with method name casing much more accurate.

--- src/org/robin/transformations/TransformationRuleDefinition.java
+++ src/org/robin/transformations/TransformationRuleDefinition.java
@@ -3,7 +3,47 @@ package org.robin.transformations;
 public class TransformationRuleDefinition {
     public static final TransformationRule[] TRANSFORMATIONS = new TransformationRule[] {
             new TransformationRule(
-                    "([\\s\\.])([A-Z])([a-zA-Z0-9_]*)\\(",
+                    "(public|protected|private)([\\s])([a-zA-Z\\<\\>\\[\\]]+)([\\s])([A-Z])([a-zA-Z0-9_]+)\\(",
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.LOWERCASE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byText("(")
+            ),
+            new TransformationRule(
+                    "^(\\s+)([A-Z])([a-zA-Z0-9_]+)\\(",
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.LOWERCASE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byText("(")
+            ),
+            new TransformationRule(
+                    "^(\\s+[a-zA-Z0-9_]+\\.)([A-Z])([a-zA-Z0-9_]+)\\(",
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.LOWERCASE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byText("(")
+            ),
+            new TransformationRule(
+                    "^(\\s+)([a-zA-Z0-9_]+\\s+)([a-zA-Z0-9_]+)(\\s+)\\=(\\s+)([A-Z])([a-zA-Z0-9_]+)\\(",
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byText("="),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.LOWERCASE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byText("(")
+            ),
+            new TransformationRule(
+                    "^(\\s+)([a-zA-Z0-9_]+)(\\s+)\\=(\\s+)([A-Z])([a-zA-Z0-9_]+)\\(",
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
+                    SectionTransformation.byText("="),
                     SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
                     SectionTransformation.byMethod(GroupTransformationMethod.LOWERCASE),
                     SectionTransformation.byMethod(GroupTransformationMethod.NO_CHANGE),
